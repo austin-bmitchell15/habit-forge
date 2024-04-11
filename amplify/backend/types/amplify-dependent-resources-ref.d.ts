0a85c58f7f2bf1d9e11590fd6770931410b7b2c4
austin-bmitchell15 <austin.bmitchell15@gmail.com>
2024-04-10 20:17:10 -0400

    amplfy- trying to reset amplify

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "habitforge2da0a577": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
