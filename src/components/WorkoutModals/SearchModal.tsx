552c2a93cdb2e56a09a4ca0a1299c0ab40bf9fa2
austin-bmitchell15 <austin.bmitchell15@gmail.com>
2024-04-21 02:45:22 -0400

    Finished some more of the Workout creator functionality.

--- src/components/WorkoutModals/SearchModal.tsx
+++ src/components/WorkoutModals/SearchModal.tsx
@@ -7,13 +7,29 @@ import {
   Input,
   Card,
   CardBody,
+  Select,
+  Option,
+  Button,
+  Typography,
 } from '@material-tailwind/react';
-import CheckCircleIcon from '@mui/icons-material/CheckCircle'; // Import a checkmark icon
-import { Exercise } from '@/API';
+import CheckIcon from '@mui/icons-material/Check'; // Check icon for selected items
+import { Exercise } from '@/API'; // Adjust if the path is different
 import ExerciseService from '@/services/ExerciseService';
 import { handler } from '@material-tailwind/react/types/components/dialog';
 import PaginationComponent from '../PaginationComponent';
-import CheckIcon from '@mui/icons-material/Check';
+import { value } from '@material-tailwind/react/types/components/chip';
+
+// Define the categories and their respective parts
+const BODY_PART_CATEGORIES: { [key: string]: string[] } = {
+  Cardio: ['Cardiovascular System'],
+  Back: ['Lats', 'Upper Back'],
+  Shoulders: ['Delts', 'Serratus Anterior'],
+  Legs: ['Abductors', 'Adductors', 'Calves', 'Glutes', 'Hamstrings', 'Quads'],
+  Arms: ['Biceps', 'Forearms', 'Triceps'],
+  Abs: ['Abs', 'Spine'],
+};
+
+const categoryKeys = Object.keys(BODY_PART_CATEGORIES); // Keys for the select options
 
 interface SearchModalProps {
   isOpen: boolean;
@@ -21,13 +37,16 @@ interface SearchModalProps {
 }
 
 export default function SearchModal({ isOpen, onClose }: SearchModalProps) {
-  const [loading, setLoading] = useState<boolean>(true);
+  const [loading, setLoading] = useState(true);
   const [error, setError] = useState<string | null>(null);
-  const [searchTerm, setSearchTerm] = useState<string>('');
+  const [searchTerm, setSearchTerm] = useState('');
   const [exercises, setExercises] = useState<Exercise[]>([]);
   const [paginatedExercises, setPaginatedExercises] = useState<Exercise[]>([]);
   const [currentPage, setCurrentPage] = useState(1);
-  const [selectedIds, setSelectedIds] = useState<Set<string>>(new Set()); // Stores the IDs of selected exercises
+  const [selectedIds, setSelectedIds] = useState<Set<string>>(new Set());
+  const [selectedCategory, setSelectedCategory] = useState<string>('');
+  const [showNamingForm, setShowNamingForm] = useState<boolean>(false);
+  const [workoutName, setWorkoutName] = useState('');
 
   useEffect(() => {
     const fetchExercises = async () => {
@@ -48,23 +67,22 @@ export default function SearchModal({ isOpen, onClose }: SearchModalProps) {
   useEffect(() => {
     setCurrentPage(1);
     updatePagination();
-  }, [searchTerm, exercises]);
-
-  useEffect(() => {
-    updatePagination();
-  }, [currentPage]);
+  }, [searchTerm, selectedCategory, exercises]);
 
   const updatePagination = () => {
-    const filteredExercises = exercises.filter((exercise) =>
-      exercise.name?.toLowerCase().includes(searchTerm.toLowerCase()),
+    const filteredExercises = exercises.filter(
+      (exercise) =>
+        exercise.name.toLowerCase().includes(searchTerm.toLowerCase()) &&
+        (selectedCategory === '' ||
+          BODY_PART_CATEGORIES[selectedCategory].includes(
+            exercise.primaryTarget,
+          )),
     );
     const itemsPerPage = 10;
     const startIndex = (currentPage - 1) * itemsPerPage;
-    const paginatedItems = filteredExercises.slice(
-      startIndex,
-      startIndex + itemsPerPage,
+    setPaginatedExercises(
+      filteredExercises.slice(startIndex, startIndex + itemsPerPage),
     );
-    setPaginatedExercises(paginatedItems);
   };
 
   const toggleSelection = (id: string) => {
@@ -82,42 +100,96 @@ export default function SearchModal({ isOpen, onClose }: SearchModalProps) {
 
   return (
     <Dialog open={isOpen} handler={onClose}>
-      <DialogHeader>
+      <DialogHeader className="flex justify-between">
         Create New Exercises ({selectedIds.size} selected)
+        {selectedIds.size > 0 && !showNamingForm && (
+          <Button
+            className="mt-4 p-2"
+            variant="outlined"
+            onClick={() => setShowNamingForm(true)}
+          >
+            Next Step
+          </Button>
+        )}
       </DialogHeader>
       <DialogBody className="flex flex-col space-y-4">
-        <Input
-          label="Search"
-          value={searchTerm}
-          onChange={(e) => setSearchTerm(e.target.value)}
-          className="flex flex-wrap justify-center gap-4"
-        />
-        <div className="flex flex-wrap justify-center gap-4">
-          {paginatedExercises.map((exercise) => (
-            <Card
-              key={exercise.id}
-              className="flex flex-grow w-1/2 cursor-pointer mx-auto"
-              onClick={() => toggleSelection(exercise.id)}
+        {showNamingForm ? (
+          <Input
+            label="Workout Name"
+            value={workoutName}
+            onChange={(e) => setWorkoutName(e.target.value)}
+          />
+        ) : (
+          <>
+            <Input
+              label="Search"
+              value={searchTerm}
+              onChange={(e) => setSearchTerm(e.target.value)}
+            />
+            <Select
+              label="Filter by Category"
+              onChange={(filter: value) => {
+                setSelectedCategory(filter as string);
+              }}
+              value={selectedCategory}
             >
-              <CardBody>
-                <div className="flex items-center justify-between">
-                  {exercise.name}
-                  {selectedIds.has(exercise?.id) && <CheckIcon />}
-                </div>
-              </CardBody>
-            </Card>
-          ))}
-        </div>
+              <Option value="">All Categories</Option>
+              <Option value="Cardio">Cardio</Option>
+              <Option value="Back">Back</Option>
+              <Option value="Shoulders">Shoulders</Option>
+              <Option value="Legs">Legs</Option>
+              <Option value="Arms">Arms</Option>
+              <Option value="Abs">Abs</Option>
+            </Select>
+            <div className="flex flex-col justify-center gap-4">
+              {paginatedExercises.map((exercise) => (
+                <Card
+                  key={exercise.id}
+                  className="p-2 cursor-pointer"
+                  onClick={() => toggleSelection(exercise.id)}
+                >
+                  <CardBody className="p-2">
+                    <div className="flex flex-row items-center justify-between">
+                      <div className="flex flex-col">
+                        <Typography variant="h6">{exercise.name}</Typography>
+                        <Typography variant="small">
+                          {exercise.primaryTarget}
+                        </Typography>
+                      </div>
+                      {selectedIds.has(exercise.id) && <CheckIcon />}
+                    </div>
+                  </CardBody>
+                </Card>
+              ))}
+            </div>
+          </>
+        )}
       </DialogBody>
-      <DialogFooter className="flex justify-center">
-        <PaginationComponent
-          items={exercises.filter((exercise) =>
-            exercise.name?.toLowerCase().includes(searchTerm.toLowerCase()),
-          )}
-          itemsPerPage={10}
-          currentPage={currentPage}
-          setCurrentPage={setCurrentPage}
-        />
+      <DialogFooter>
+        {showNamingForm ? (
+          <Button
+            color="green"
+            onClick={() => console.log(`Workout Named: ${workoutName}`)}
+          >
+            Save Workout
+          </Button>
+        ) : (
+          <PaginationComponent
+            items={exercises.filter(
+              (exercise) =>
+                exercise.name
+                  .toLowerCase()
+                  .includes(searchTerm.toLowerCase()) &&
+                (selectedCategory === '' ||
+                  BODY_PART_CATEGORIES[selectedCategory].includes(
+                    exercise.primaryTarget,
+                  )),
+            )}
+            itemsPerPage={10}
+            currentPage={currentPage}
+            setCurrentPage={setCurrentPage}
+          />
+        )}
       </DialogFooter>
     </Dialog>
   );
---
+++ src/utils/types/exercises.ts
@@ -0,0 +1,17 @@
+export const BODY_PART_CATEGORY_MAPPING = {
+  Cardio: ['Cardiovascular System'],
+  Back: ['Lats', 'Upper Back'],
+  Shoulders: ['Delts', 'Serratus Anterior'],
+  Legs: ['Abductors', 'Adductors', 'Calves', 'Glutes', 'Hamstrings', 'Quads'],
+  Arms: ['Biceps', 'Forearms', 'Triceps'],
+  Abs: ['Abs', 'Spine'],
+};
+
+export const BODY_PART_CATEGORIES = [
+  'Cardio',
+  'Back',
+  'Shoulders',
+  'Legs',
+  'Arms',
+  'Abs',
+];
